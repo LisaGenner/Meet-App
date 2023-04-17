3b03b2bc473e9996163dca9b8947378905255705
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-04-17 14:04:02 -0500

    Update handler.js
    updated handler details

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,15 +1,9 @@
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
-/**
- * SCOPES allows you to set access levels; this is set to read only for now because you don't have access rights to
- * update the calendar yourself. For more info, check out the SCOPES documentation at this link: https://developers.google.com/identity/protocols/oauth2/scopes
- */
+
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-/**
- * Credentials are values required to get access to your calendar. If you see “process.env” this means
- * the value is in the “config.json” file. This is a best practice as it keeps your API secrets hidden. Please remember to add “config.json” to your “.gitignore” file.
- */
+
 const credentials = {
   client_id: process.env.CLIENT_ID,
   project_id: process.env.PROJECT_ID,
@@ -19,14 +13,10 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://lisagenner.github.io/Meet-App/"],
-  javascript_origins: ["https://LisaGenner.github.io", "http://localhost:3001"],
+  javascript_origins: ["https://lisagenner.github.io", "http://localhost:3001"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
-const oAuth2Client = new google.auth.OAuth2(
-  client_id,
-  client_secret,
-  redirect_uris[0]
-);
+const oAuth2Client = new OAuth2(client_id, client_secret, redirect_uris[0]);
 
 module.exports.getAuthURL = async () => {
   const authUrl = oAuth2Client.generateAuthUrl({
