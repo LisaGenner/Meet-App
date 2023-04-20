7720613803014c0c3d8c7143d58b0962fe599288
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-04-20 11:37:45 -0500

    4.3

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -16,7 +16,11 @@ const credentials = {
   javascript_origins: ["https://lisagenner.github.io", "http://localhost:3001"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
-const oAuth2Client = new OAuth2(client_id, client_secret, redirect_uris[0]);
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+);
 
 module.exports.getAuthURL = async () => {
   const authUrl = oAuth2Client.generateAuthUrl({
